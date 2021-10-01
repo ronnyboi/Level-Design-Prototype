73cddf11492bdc49f95efe626329ba2ab38cab0a
ronnyboi <42110292+ronnyboi@users.noreply.github.com>
2021-10-02 02:05:00 +0530

    first commit

---
+++ Assets/Level Design/scripts/bulbFlicker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulbFlicker : MonoBehaviour {
+    public AudioClip clip;
+    bool canFlicker = true;
+    AudioSource audio;
+
+    void Update()
+    {
+        StartCoroutine(Flicker());
+
+    }
+
+    IEnumerator Flicker()
+    {
+        if (canFlicker)
+        {
+            canFlicker = false;
+            audio = GetComponent<AudioSource>();
+            audio.PlayOneShot(clip);
+            GetComponent<Light>().GetComponent<Light>().enabled = true;
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.4f));
+            GetComponent<Light>().GetComponent<Light>().enabled = false;
+            yield return new WaitForSeconds(Random.Range(0.1f, 5));
+            canFlicker = true;
+        }
+    }
+}
